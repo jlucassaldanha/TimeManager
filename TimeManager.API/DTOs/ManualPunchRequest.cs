0037f76f47e2ab4b9742e7be532a6c92b0fec9b1
João Lucas Saldanha <jlucassaldanha@gmail.com>
2026-05-07 11:26:08 -0300

    Adiciona controller de ponto manual

---
+++ TimeManager.API/DTOs/ManualPunchRequest.cs
@@ -0,0 +1,10 @@
+using TimeManager.Domain.Entities;
+
+namespace TimeManager.API.DTOs;
+
+public class ManualPunchRequest {
+	public required Guid UserId { get; set;} 
+	public required DateTime DateTime { get; set;}  
+	public required RecordType Type { get; set;} 
+	public required string Note { get; set;}
+}
